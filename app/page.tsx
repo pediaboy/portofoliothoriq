8a95255e0b9ca110a6a61838294a8f51d3e76041
pediaboy <thoriqpedia@gmail.com>
2026-06-22 02:50:37 +0700

    fix: all sections dynamic ssr:false to prevent hydration crash

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
+'use client'
+
 import dynamic from 'next/dynamic'
-import HeroSection from './sections/HeroSection'
-import AboutSection from './sections/AboutSection'
-import SkillsSection from './sections/SkillsSection'
-import PortfolioSection from './sections/PortfolioSection'
-import MarketSection from './sections/MarketSection'
-import StatsSection from './sections/StatsSection'
-import ContactSection from './sections/ContactSection'
 
 const GalaxyCanvas = dynamic(() => import('./components/GalaxyCanvas'), { ssr: false })
 const LoadingScreen = dynamic(() => import('./components/LoadingScreen'), { ssr: false })
 const Navbar = dynamic(() => import('./components/Navbar'), { ssr: false })
 const CursorGlow = dynamic(() => import('./components/CursorGlow'), { ssr: false })
+const HeroSection = dynamic(() => import('./sections/HeroSection'), { ssr: false })
+const AboutSection = dynamic(() => import('./sections/AboutSection'), { ssr: false })
+const SkillsSection = dynamic(() => import('./sections/SkillsSection'), { ssr: false })
+const PortfolioSection = dynamic(() => import('./sections/PortfolioSection'), { ssr: false })
+const MarketSection = dynamic(() => import('./sections/MarketSection'), { ssr: false })
+const StatsSection = dynamic(() => import('./sections/StatsSection'), { ssr: false })
+const ContactSection = dynamic(() => import('./sections/ContactSection'), { ssr: false })
 
 export default function Home() {
   return (
